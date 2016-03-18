ac6ced67fbc0527480748316c0f73f3a0f4eb91e
david jones <david@rackfx.com>
2016-03-17 18:52:21 -0600

    -

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var moment = require('moment-timezone');
 var mysql      = require('mysql');
 var connection = mysql.createConnection(require('./config/database.js'));
-
+var colors = require('colors');
 var express = require('express');
 var app = express();
 
@@ -11,18 +11,31 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
+
+
 app.listen(3000, function () {
   console.log('app listening on port 3000!');
 });
 
 
 setInterval(function(){ console.log('log'); log(); }, 15000);
-
+log();
 function log(){
   var post = { timestamp : moment().format() }; 
   var query = connection.query('INSERT INTO log SET ?', post, function(err, result) {
-	console.log(err, result);  			// Neat!	 
-		});
+	console.log('logged id', result.insertId.toString().red, 'at', post.timestamp.blue);  			// Neat!	 	
+  });
 
 
 }
+
+
+// Blink an LED
+var five = require("johnny-five");
+var board = new five.Board({repl:false});
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+  setInterval(function(){  led.toggle(); console.log('blink');} , 250);
+});
